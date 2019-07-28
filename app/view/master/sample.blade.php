8936e47a697176747e265c3f1b05b38717968709
mehmetalikus <mehmetali.kus@hotmail.com>
2019-07-28 23:58:30 +0300

    javascript init

--- app/view/master/sample.blade.php
+++ app/view/master/sample.blade.php
@@ -11,6 +11,13 @@
 	
 	<h1>Sample Page</h1>
 
+	<script type="text/javascript">
+		let init = {
+			base_url: "<?=site_url()?>",
+			ajax: "<?=site_url("ajax")?>",
+			language: "<?=app("language")?>"
+		};
+	</script>
 	<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="<?=assets_url("js/main.js")?>"></script>
